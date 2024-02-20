dd7b4dd39918c41f2d26971ad0c84cd9d85fd09c
Ayush Bikram <ghimireayush52@gmail.com>
2024-02-20 12:37:37 +0545

    1

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ from tkinter import ttk, scrolledtext, messagebox, filedialog, Menu
 import socket
 import threading
 import ipaddress
+
+# Function to validate an IP address
+def valid_ip(address):
+    try:
+        ipaddress.ip_address(address)
+        return True
+    except ValueError:
+        return False
